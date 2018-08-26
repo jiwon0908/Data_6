19a5b5a9357d251274183f43aac277f8562764b6
korra0501 <korra0501@gmail.com>
2018-08-26 15:42:05 +0900

    홈페이지 추가, DB 연결중, 404페이지 수정
    자바스크립트, CSS 수정
    해야할 일
    - DB연결, 띄우는 작업
    - 각 복지관 별 설명(DETAIL PAGE)
    - 소셜네트워크 연결

--- BIGDATA_Project2/app.py
+++ BIGDATA_Project2/app.py
@@ -1,11 +1,53 @@
+# 설치해야 할 외부 라이브러리
 from flask import Flask, render_template, url_for
+from flask_mail import Mail
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# 프로그램 내의 파이썬 파일
+from config import Config
+from database import *
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+admin = Admin(app, name='microblog', template_mode='bootstrap4')
+# Add administrative views here
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # 캐시 설정
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB.db'
+db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = '빅데이터고려대학교6조'
+app.config.from_object(Config)
+mail = Mail(app)
+
+
+class User(db.Model):
+    __tablename__ = "user_data"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.Text(10), unique=False, nullable=False)
+    culture_view = db.Column(db.Float(20), unique=False, nullable=False)
+    culture_parti = db.Column(db.Float(20), unique=False, nullable=False)
+    sport_view = db.Column(db.Float(20), unique=False, nullable=False)
+    sport_parti = db.Column(db.Float(20), unique=False, nullable=False)
+    sightsee = db.Column(db.Float(20), unique=False, nullable=False)
+    entertain = db.Column(db.Float(20), unique=False, nullable=False)
+    rest = db.Column(db.Float(20), unique=False, nullable=False)
+    social_act = db.Column(db.Float(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+#me = User()
+#db.session.add(me)
+#db.session.commit()
 
 @app.route('/')
 def show_home():
-    return render_template('index.html')
+    return render_template('coming_soon.html')
 
 @app.route('/faq')
 def faq():
---
+++ BIGDATA_Project2/database.py
@@ -0,0 +1,7 @@
+#import pandas as pd
+#import numpy as np
+#from app import User
+
+#me = User('admin', 'admin@example.com')
+#db.session.add(me)
+#db.session.commit()
