af2470731e6ca22397d28fba06824b5b89828e79
korra0501 <38252899+korra0501@users.noreply.github.com>
2019-04-26 02:51:04 +0900

    bug fix
    각 페이지에서 동행 마크를 누르면 홈페이지로 연결되지 않던 버그 수정

--- BIGDATA_Project2/app.py
+++ BIGDATA_Project2/app.py
@@ -383,7 +383,7 @@ def faq():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html')
+    return render_template('404.html'), 404
 
 @app.route('/<path>')
 def path(path):
